6623f7d472c4468a03865ec8224d09d0f160bb76
tangxuandong <jason.tangxd@gmail.com>
2019-05-29 14:02:25 +0800

    init

---
+++ src/main/java/com/xiaoxiaomo/eureka/EurekaServer.java
@@ -0,0 +1,19 @@
+package com.xiaoxiaomo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka注册中心
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer.class, args);
+	}
+
+}
